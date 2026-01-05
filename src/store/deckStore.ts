21f18dc8cf4c86c31714c3f513a0793e4a8e9676
David Felipe <felipe30.10@outlook.com>
2026-01-05 17:55:30 -0300

    add deckStore

---
+++ src/store/deckStore.ts
@@ -0,0 +1,90 @@
+import { create } from "zustand"
+
+type Card = {
+  id: string
+  question: string
+  answer: string
+}
+
+type Deck = {
+  id: string
+  title: string
+  cards: Card[]
+}
+
+type Store = {
+  decks: Deck[]
+
+  createDeck: (title: string) => void
+  deleteDeck: (id: string) => void
+
+  addCard: (deckId: string, q: string, a: string) => void
+  editCard: (deckId: string, cardId: string, q: string, a: string) => void
+  removeCard: (deckId: string, cardId: string) => void
+}
+
+export const useDeckStore = create<Store>((set) => ({
+  decks: [],
+
+  createDeck: (title) =>
+    set((state) => ({
+      decks: [
+        ...state.decks,
+        { id: Date.now().toString(), title, cards: [] },
+      ],
+    })),
+
+  deleteDeck: (id) =>
+    set((state) => ({
+      decks: state.decks.filter((deck) => deck.id !== id),
+    })),
+
+  addCard: (deckId, question, answer) =>
+    set((state) => ({
+      decks: state.decks.map((deck) =>
+        deck.id === deckId
+          ? {
+              ...deck,
+              cards: [
+                ...deck.cards,
+                {
+                  id: Date.now().toString(),
+                  question: question,
+                  answer: answer,
+                },
+              ],
+            }
+          : deck
+      ),
+    })),
+
+    editCard: (deckId, cardId, question, answer) =>
+    set((state) => ({
+      decks: state.decks.map((deck) =>
+        deck.id === deckId
+          ? {
+              ...deck,
+              cards: deck.cards.map((card) =>
+                card.id === cardId
+                  ? { ...card, question: question, answer: answer }
+                  : card
+              ),
+            }
+          : deck
+      ),
+    })),
+
+    removeCard: (deckId, cardId) =>
+    set((state) => ({
+      decks: state.decks.map((deck) =>
+        deck.id === deckId
+          ? {
+              ...deck,
+              cards: deck.cards.filter((card) => card.id !== cardId),
+            }
+          : deck
+      ),
+    })),
+    
+}))
+
